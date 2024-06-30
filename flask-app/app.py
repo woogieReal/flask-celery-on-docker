9f1f8a4773e4ebb51c9629d1e0a0dc057dc65ce3
woogieReal <woogiereal@gmail.com>
2024-06-30 21:17:52 +0900

    feat: add get_api_logs api

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,11 +1,13 @@
 from database import db_session, init_db
 from flask import Flask, request, jsonify
 from celery import Celery
+from sqlalchemy import desc
+from flask_cors import CORS
 from models import APILog
 import os
-import json
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 app.config['CELERY_BROKER_URL'] = os.environ['CELERY_BROKER_URL']
 app.config['CELERY_RESULT_BACKEND'] = os.environ['CELERY_RESULT_BACKEND']
@@ -37,6 +39,21 @@ def add_to_queue():
     print(api_info, flush=True)
     process_data.delay(api_info)
     return jsonify({'message': 'Data added to queue'}), 202
+
+@app.route('/api_logs', methods=['GET'])
+def get_api_logs():
+   # 세션을 통해 데이터베이스에서 모든 로그 조회
+    logs = db_session.query(APILog).order_by(desc(APILog.creation_date)).all()
+    # 조회된 로그를 리스트로 변환
+    logs_list = [
+        {
+            'logId': log.log_id,
+            'apiRequest': log.api_request,
+            'apiResponse': log.api_response,
+            'creationDate': log.creation_date
+        } for log in logs
+    ]
+    return jsonify(logs_list)
 
 if __name__ == '__main__':
     init_db()
